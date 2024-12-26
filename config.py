82d41ade2bd29ef6cbe1293b36127096b9236b25
MN - TG <mntg3509@gmail.com>
2024-12-26 16:16:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b> Hello, {mention}\n\nJoin our amazing channels to unlock exclusive content! After joining, simply hit the reload button to access your requested file. Let’s get started!</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b> previouscaption} </b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b> {previouscaption} </b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
